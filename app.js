009168d1d1fda330628024ded0bc07f1929b70ff
Kartik <kartik.gavali@thynqit.com>
2026-03-27 11:00:03 +0530

    feat: add version 5

--- app.js
+++ app.js
@@ -64,5 +64,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} v3`);
+  console.log(`Server running on port ${PORT}`);
 });
